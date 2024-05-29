c9c16a65bd1123b87e308689a59a565b65ef9d5c
Tiantian Li <tiantiancscs@gmail.com>
2024-05-29 18:35:49 +1000

    feat: update password service
    Added the resolver and service of changePassword:
    - get ID of the current logged in user;
    - verify current password;
    - replace old password with new one
    - add password pattern validation pipe
    
    Added unit test:
    - add test for changePassword service
    - add test for password pattern validation pipe
    
    Resolve CP-34

--- src/common/constants/code.ts
+++ src/common/constants/code.ts
@@ -6,3 +6,5 @@ export const ACCOUNT_EXIST = 10004;
 export const REGISTER_ERROR = 10005;
 export const NOT_EMPTY = 10006;
 export const VALIDATE_ERROR = 10007;
+export const UPDATE_PASSWORD_ERROR = 10008;
+export const UNKNOWN_ERROR = 10009;
--- src/modules/auth/auth.resolver.ts
+++ src/modules/auth/auth.resolver.ts
@@ -1,12 +1,16 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { UsePipes } from '@nestjs/common';
-import { UseFilters } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { UsePipes, UseFilters, UseGuards } from '@nestjs/common';
 import { CreateUserInput } from '../user/dto/new-user.input';
 import { AuthService } from './auth.service';
 import { ValidationPipe } from '@/modules/auth/pipe/registration-validation.pipe';
 import { userSchema } from '../../validation/schemas/auth/user.request';
 import { Result } from '@/common/dto/result.type';
 import { RegisterPipeErrorFilter } from './filter/register-pipe-error.filter';
+import { passwordUpdateSchema } from '@/validation/schemas/auth/password.update';
+import { UpdatePasswordInput } from '../user/dto/update-password.input';
+import { PasswordValidationPipe } from './pipe/password-validation.pipe';
+import { PasswordPipeErrorFilter } from './filter/password-pipe-error.filter';
+import { GqlAuthGuard } from '@/common/guards/auth.guard';
 
 @Resolver()
 export class AuthResolver {
@@ -25,5 +29,15 @@ export class AuthResolver {
   @UseFilters(RegisterPipeErrorFilter)
   async register(@Args('input') input: CreateUserInput): Promise<Result> {
     return await this.authService.register(input);
+  }
+
+  @Mutation(() => Result, { description: 'Change password' })
+  @UseFilters(PasswordPipeErrorFilter)
+  @UseGuards(GqlAuthGuard)
+  async changePassword(
+    @Context() cxt: { req: Partial<Request> & {user: {id: string}} },
+    @Args('input', new PasswordValidationPipe(passwordUpdateSchema)) input: UpdatePasswordInput,
+  ): Promise<Result> {
+    return await this.authService.changePassword(cxt, input);
   }
 }
