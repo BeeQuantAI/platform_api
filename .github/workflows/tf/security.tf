95f6e059d9b8b1a2a2880396bdffabf4dd08acad
Q12976433 <53292933+Q12976433@users.noreply.github.com>
2024-06-13 03:45:15 +1000

    Update security.tf

--- .github/workflows/tf/security.tf
+++ .github/workflows/tf/security.tf
@@ -23,6 +23,34 @@ ingress {
   }
 }
 
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Security group for the Application Load Balancer"
+  vpc_id      = aws_vpc.bqcore_vpc.id  # Ensure this is the correct VPC ID from your setup
+
+  # Inbound rules: Typically, allow HTTP and HTTPS traffic
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  # Outbound rules: Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALB Security Group"
+  }
+}
+
+
 
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs_security_group"
