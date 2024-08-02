9d5842b5c47b1cb6490160e37941e3624a162851
SeijaHuang <hsj12544975@gmail.com>
2024-08-02 19:53:38 +0800

    Refactor: refactor password validation pipe
    Update
    - make errorMatcher function shared between password and registration validation pipe
    - update errorMatcher to catch zod validation errors into corresponding HttpException
    - update password.update and user.request schema using zod library and responding test files
    Refactor
    - refactor password-validation and registration-validation pipe and their test files
    Resolve CP-90

--- src/modules/auth/pipe/registration-validation.pipe.ts
+++ src/modules/auth/pipe/registration-validation.pipe.ts
@@ -1,33 +1,24 @@
-import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
-import * as Joi from 'joi';
-import { EmptyFiledException } from '../../../exceptions/empty-field.exception';
-import { InvalidInputException } from '../../../exceptions/invalid-input.exception';
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { errorMatcher } from '@/common/utils/errorMatcher';
+import { z, ZodIssue, ZodSchema } from 'zod';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
-  constructor(private schema: Joi.ObjectSchema) {}
+  constructor(private schema: ZodSchema) {}
 
-  transform(value: any) {
-    const { error, value: validatedValue } = this.schema.validate(value);
-    if (error) {
-      const path = error.details[0].path[0];
-      const type = error.details[0].type;
-      const message = error.details[0].message;
-      this.errorMatcher(path, type, message);
-    }
-    return validatedValue;
-  }
-
-  private errorMatcher(path, type, message) {
-    switch (type) {
-      case 'string.pattern.base':
-        throw new InvalidInputException(message);
-      case 'any.required':
-        throw new EmptyFiledException(`"${path}" is required`);
-      case 'string.email':
-        throw new InvalidInputException(`"${path}" must be a valid email`);
-      default:
-        throw new BadRequestException(message);
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      const issues: ZodIssue = error.issues;
+      const code: z.ZodIssueCode = issues[0].code;
+      const message: string = issues[0].message;
+      if (code === 'too_small') {
+        const min: number = issues[0].minimum;
+        return errorMatcher({ code, message, min });
+      }
+      return errorMatcher({ code, message });
     }
   }
 }
