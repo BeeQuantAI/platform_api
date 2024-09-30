1e3f73d3061828e94a38e971feb794b330953bee
Shiyu <chengjjy16@gmail.com>
2024-09-30 23:24:37 +1000

    feat: add exchange management feature
    Implemented functionality to retrieve user's current exchanges and balances, convert balances into USD, and calculate the total USD value for different cryptocurrencies.
    
    Resolve CP-14
    
    Implemented functionality to retrieve platform names and the balance information for the user's exchanges. Balances are converted into USD, and the total USD value for different cryptocurrencies is calculated.
    
    Delete unnecessary console log message
    
    Delete unused file and adding up USD values
    
    Added the ability to get user information directly from the context without the need for manual input.
    
    Add exchange id into query for subsequent connection to exchange update
    
    Add displayName into result
    
    - Solved the problem of long fetch time
    - Solved the problem of getting wrong exchangeId.

---
+++ src/modules/user-exchange/dto/userExchangeResult.type.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { createResults } from '@/common/dto/result.type';
+
+@ObjectType()
+export class UserExchangeType {
+  @Field({ description: 'Exchange ID' })
+  id: string;
+
+  @Field({ description: 'Exchange name' })
+  name: string;
+
+  @Field({ description: 'Display Name' })
+  displayName: string;
+
+  @Field({ description: 'Total balance in USD' })
+  balances: number;
+}
+
+export const ResultForExchanges = createResults(UserExchangeType);
